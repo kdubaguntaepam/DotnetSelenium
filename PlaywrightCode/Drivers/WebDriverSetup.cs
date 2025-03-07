9ecaaaf89675f9ead0bf91050bf1d67dbdaa9c8f
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2025-03-07 13:57:04 +0530

    Create PlaywrightCode/Drivers/WebDriverSetup.cs

---
+++ PlaywrightCode/Drivers/WebDriverSetup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace SwagLabProject.PlaywrightCode.Drivers
+{
+    public class WebDriverSetup
+    {
+        protected IBrowser Browser;
+        protected IPage Page;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var playwright = await Playwright.CreateAsync();
+            Browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            Page = await Browser.NewPageAsync();
+            await Page.GotoAsync("https://www.saucedemo.com/");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Browser.CloseAsync();
+        }
+    }
+}
